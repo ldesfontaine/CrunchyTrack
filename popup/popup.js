166c1870a3e356ebed791413137fde7444e5bb0a
lucas.desfontaine <lucas.desfontaine1@gmail.com>
2023-05-21 08:06:10 +1100

    feat(redirect): Ajout redirection si pas sur crunchy

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,21 @@
-document.getElementById('bouton').addEventListener('click', function() {
-    if (window.location.href !== 'www.crunchyroll.com') {
-        window.location.href = 'www.crunchyroll.com';
-    } else {
-        alert('Bravo');
-    }
+// Main function
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        var currentTab = tabs[0];
+        if (currentTab.url.includes('crunchyroll.com')) {
+            alert('This extension work on Crunchyroll.com');
+        } else {
+            showRedirectButton();
+        }
+    });
 });
+
+function showRedirectButton() {
+    var notCrunchyDiv = document.getElementById('not-crunchy');
+    notCrunchyDiv.style.display = 'block';
+
+    var redirectButton = document.getElementById('redirect');
+    redirectButton.addEventListener('click', function() {
+        chrome.tabs.create({ url: 'https://www.crunchyroll.com/' });
+    });
+}
